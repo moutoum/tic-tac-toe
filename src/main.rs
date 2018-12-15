dba3457183b442a69984e555a6d824ed849798e2
Maxence Moutoussamy <maxence.moutoussamy@epitech.eu>
2018-12-15 20:25:30 +0100

    add SDL2 dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// File: main.rs
+// File Created: 15 Dec 2018 20:08
+// By Maxence Moutoussamy <maxence.moutoussamy1@gmail.com>
+
 fn main() {
-    println!("Hello, world!");
+    println!("Tic tac toe");
 }
